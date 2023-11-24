535a01375a109fd3600a1d8f0ddde1bcafd6c090
rhyrhy <rhy123123@naver.com>
2023-11-24 14:52:39 +0900

    records.h

---
+++ headers/records.h
@@ -0,0 +1,16 @@
+#ifndef RECORDS_H
+#define RECORDS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "types.h"
+
+void getPlayerNames();
+void saveRecords();
+int  recordsCount();
+void retrieveRecords();
+void showRecords();
+
+
+#endif
