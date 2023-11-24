d965295a4328ff5dfbbdc54e9519dc8ed88cd41e
rhyrhy <rhy123123@naver.com>
2023-11-24 14:53:04 +0900

    seperated types

---
+++ headers/types.h
@@ -0,0 +1,23 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// 키보드 입력값
+typedef enum {
+  NONE = 0,
+  LEFT_1P, RIGHT_1P,
+  LEFT_2P, RIGHT_2P
+} Dir;
+
+// 좌표 관련
+typedef struct {
+  int x;
+  int y;
+} Point, Vector;
+
+typedef struct {
+  char name[50];
+  int score;
+} player;
+
+
+#endif
