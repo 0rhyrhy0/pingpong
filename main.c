c30552f545ac644833443f1d243ac2643fe3bd1e
rhyrhy <rhy123123@naver.com>
2023-11-25 14:36:25 +0900

    게임 종료 이후 출력내용 수정

--- main.c
+++ main.c
@@ -105,18 +105,21 @@ int main() {
   printf("Submit your scores? [y/n] ");
   char x;
   while((x = getchar_()) != 'y' && x != 'n');
-  puts("");
   if(x == 'y') {
+    puts("");
     getPlayerNames();
     saveRecords();
   }
+  
   // 이후 recordsCount를 통해 기록 개수 파악 후 출력
   int count = recordsCount();
   gameResult *results = malloc(sizeof(gameResult) * count);
-  retrieveRecords(results, count);
-  
-  system("clear");
-  showRecords(results, count);
-
+  if(count) {
+    puts("");
+    retrieveRecords(results, count);
+    
+    // system("clear");
+    showRecords(results, count);
+  }
   return 0;
 }
